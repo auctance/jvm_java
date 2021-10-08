eaf3ad8e0095f1d2c2ed8be12e896fc9bf62afdf
auctance <liuanqi0424@126.com>
2021-10-08 23:03:24 +0800

    指令解析，class文件中对应解析指令，完成操作

--- src/instructions/base/Instruction.java
+++ src/instructions/base/Instruction.java
@@ -1,9 +1,9 @@
 // 定义指令接口 从字节码中读取指令 定义一个结构体来辅助指令解码
 
 // 会被每个具体解码的对象 实例化
-
+package instructions.base;
 import instructions.base.BytecodeReader;
-
+import runtimedata.Zframe;
 // 所有指令解析 都需要完成读取操作数 以及 执行栈帧中指令的逻辑
 public interface Instruction {
     // 从字节码中取操作数
